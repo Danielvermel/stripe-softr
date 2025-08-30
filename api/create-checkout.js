6a997018c524cb9e11deb73b64be0b5a691f5879
Daniel Leite <daniel.f.m.leite@gmail.com>
2025-08-30 02:30:59 +0100

    Add onboarding URL to Airtable for easy practitioner access

---
+++ api/create-checkout.js
@@ -0,0 +1,7 @@
+{
+    "practitionerId": "recXXXXXXXXXXXXXX", // Links to Stripe account we created
+    "patientEmail": "patient@email.com",
+    "consultationPrice": 15000, // £150.00 in pence
+    "platformCommission": 1500, // £15.00 commission (10%)
+    "consultationDetails": "60min therapy session"
+  }
